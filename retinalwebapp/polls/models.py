3dd4e40f45e968ba9707d9aeeb41490fd76020dd
Tanmay Kulkarni <iamtanmaykulkarni@gmail.com>
2015-02-19 16:47:51 +0530

    question objects are displayed by their question_text in the django admin

--- retinalwebapp/polls/models.py
+++ retinalwebapp/polls/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
-      question_text = models.CharField(max_length=200)
-      pub_date = models.DateTimeField('date published')
+  def __unicode__(self):
+    # return "Question " + str(self.id)
+    return self.question_text
+  question_text = models.CharField(max_length=200)
+  pub_date = models.DateTimeField('date published')
 
 class Choice(models.Model):
-      question = models.ForeignKey(Question)
-      choice_text = models.CharField(max_length=200)
-votes = models.IntegerField(default=0)
+  question = models.ForeignKey(Question)
+  choice_text = models.CharField(max_length=200)
+  votes = models.IntegerField(default=0)

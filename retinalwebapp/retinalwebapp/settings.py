fdbb5fc51e98966baf94d6270d41fd143b868396
Tanmay Kulkarni <iamtanmaykulkarni@gmail.com>
2015-02-20 17:34:19 +0530

    page ready for templates

--- retinalwebapp/retinalwebapp/settings.py
+++ retinalwebapp/retinalwebapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls',
+    'retinalwebapp',
 )
 
 MIDDLEWARE_CLASSES = (
--- retinalwebapp/retinalwebapp/urls.py
+++ retinalwebapp/retinalwebapp/urls.py
@@ -1,9 +1,25 @@
+'''
+  The page heirarchy of the webapp is as follows:
+  
+                   IlabelIT
+                   /      \
+                 Login  Signup
+                 /   \
+               Quiz  Abnormalities
+                
+
+'''
+
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+
+import retinalwebapp.views
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'retinalwebapp.views.home', name='home'),
+    url(r'^$login', 'retinalwebapp.views.login', name='login'),
+    url(r'^$signup', 'retinalwebapp.views.signup', name='signup'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
